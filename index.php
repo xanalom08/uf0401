742770f13c94fed8e34055d8640a70cbf0bda850
Xavier Navarro López <xanalom08@gmail.com>
2017-05-21 21:31:29 +0200

    Project uf0401 version 0.2

--- index.php
+++ index.php
@@ -7,11 +7,19 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Prova</title>
     </head>
     <body>
+        <p>Holaaaaaa</p>
         <?php
-         Echo "Hello, World!!";
+        function setHeight($minheight = 50) {
+            echo "The height is : $minheight <br>";
+        }
+
+        setHeight(350);
+        setHeight(); // will use the default value of 50
+        setHeight(135);
+        setHeight(80);
         ?>
     </body>
 </html>
